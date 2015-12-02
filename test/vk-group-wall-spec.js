b5b909cf5b070cf7207657a9214cf80e3f8ead10
ewnd9 <ewndnine@gmail.com>
2015-12-03 02:10:19 +0800

    Add "vk group wall" task

---
+++ test/vk-group-wall-spec.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+import 'babel-core/register';
+import task from './../src/tasks/vk-group-wall-task';
+
+test('follow specification', t => {
+	t.true(typeof task.task === 'function');
+});
+
+// test('get search results', async t => {
+// 	const owner = '-1';
+// 	const items = await task.task(owner);
+// 	setTimeout(() => t.fail(), 1000);
+// });
