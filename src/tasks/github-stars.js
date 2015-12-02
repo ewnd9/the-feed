5a3da8613c03987b5c0d5f69725d87bb8343335a
ewnd9 <ewndnine@gmail.com>
2015-12-03 00:19:21 +0800

    Add "made public" event to github stars task

--- src/tasks/github-stars.js
+++ src/tasks/github-stars.js
@@ -2,26 +2,34 @@ const got = require('got');
 const TOKEN_ENV_VARIABLE = 'GITHUB_TOKEN';
 const token = process.env[TOKEN_ENV_VARIABLE];
 
+const processEvents = (items, filterFn, action) => {
+	const mapFn = (action) => {
+		return (item) => ({
+			id: item.id,
+			user_url: `https://github.com/${item.actor.login}`,
+			title: `${item.actor.login} ${action} ${item.repo.name}`,
+			url: `https://github.com/${item.repo.name}`
+		});
+	};
+
+	return items.filter(filterFn).map(mapFn(action));
+};
+
 export const getStars = (items) => {
-	return items.filter((item) => {
-		return item.type === 'WatchEvent' && item.payload.action === 'started';
-	}).map((item) => ({
-		id: item.id,
-		user_url: `https://github.com/${item.actor.login}`,
-		title: `${item.actor.login} starred ${item.repo.name}`,
-		url: `https://github.com/${item.repo.name}`
-	}));
+	const filterFn = (item) => item.type === 'WatchEvent' &&
+														 item.payload.action === 'started';
+	return processEvents(items, filterFn, 'starred');
 };
 
 export const getRepoCreations = (items) => {
-	return items.filter((item) => {
-		return item.type === 'CreateEvent' && item.payload.ref_type === 'repository';
-	}).map((item) => ({
-		id: item.id,
-		user_url: `https://github.com/${item.actor.login}`,
-		title: `${item.actor.login} created ${item.repo.name}`,
-		url: `https://github.com/${item.repo.name}`
-	}));
+	const filterFn = (item) => item.type === 'CreateEvent' &&
+														 item.payload.ref_type === 'repository';
+	return processEvents(items, filterFn, 'created');
+};
+
+export const getRepoMadePublics = (items) => {
+	const filterFn = (item) => item.type === 'PublicEvent';
+	return processEvents(items, filterFn, 'made public');
 };
 
 const task = () => {
@@ -32,7 +40,8 @@ const task = () => {
 			'authorization': 'token ' + token
 		}
 	}).then((res) => {
-		return getStars(res.body).concat(getRepoCreations(res.body));
+		return getStars(res.body).concat(getRepoCreations(res.body))
+														 .concat(getRepoMadePublics(res.body));
 	});
 };
 
