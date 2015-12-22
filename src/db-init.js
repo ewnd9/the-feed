71a8030038bcd05f9336763ad9ea99f3c290871e
ewnd9 <ewndnine@gmail.com>
2015-12-23 05:33:47 +0800

    refactor pouch api

--- src/db-init.js
+++ src/db-init.js
@@ -1,49 +1,50 @@
-var PouchDB = require('pouchdb');
+import path from 'expand-tilde';
+import Promise from 'bluebird';
+import _ from 'lodash';
+
+import PouchDB from 'pouchdb';
 PouchDB.plugin(require('pouchdb-hoodie-api'));
 PouchDB.plugin(require('pouchdb-find'));
 
-var path = require('expand-tilde');
-
-var createDesignDoc = (name, mapFunction) => {
-  var ddoc = {
+const createDesignDoc = (name, mapFunction) => {
+  const ddoc = {
     _id: '_design/' + name,
     views: {}
   };
 
 	ddoc.views[name] = { map: mapFunction.toString() };
   return ddoc;
-}
+};
 
-var updatedAtIndex = createDesignDoc('by_updated_at', function (doc) {
-  emit(doc.updatedAt, doc);
-});
-var createdAtIndex = createDesignDoc('by_created_at', function (doc) {
-  emit(doc.createdAt, doc);
-});
+export const BY_CREATED_AT = 'by_created_at';
+export const BY_CATEGORY = 'by_category';
+
+const indexes = [
+  createDesignDoc(BY_CREATED_AT, (doc) => {
+    emit([doc.createdAt, doc._id]);
+  }),
+  createDesignDoc(BY_CATEGORY, (doc) => {
+    emit(doc.meta.task + '$' + doc.createdAt + '$' + doc._id);
+  })
+];
 
 export default (dbPath) => {
-  var pouch = new PouchDB(path(dbPath));
-  var db = pouch.hoodieApi({});
+  const pouch = new PouchDB(path(dbPath));
+  const db = pouch.hoodieApi({});
 
-	return pouch.put(updatedAtIndex).then((doc) => {
-		console.log('updatedAt index created');
-	}, (err) => {
-		if (err.name !== 'conflict') {
-	  	console.log(err);
-		} else {
-      console.log('updatedAt index exists');
-    }
-  }).then(() => {
-    return pouch.put(createdAtIndex);
-  }).then((doc) => {
-    console.log('createdAt index created');
-	}, (err) => {
-    if (err.name !== 'conflict') {
-      console.log(err);
-    } else {
-      console.log('createdAt index exists');
-    }
-  }).then(() => ({
-		pouch, db
-	}));
+  return Promise
+    .map(indexes, (index) => {
+      return pouch.put(index).then((doc) => {
+        console.log(`${index._id} was created`);
+      }, (err) => {
+        if (err.name !== 'conflict') {
+    	  	console.log(err);
+    		} else {
+          console.log(`${index._id} already exists`);
+        }
+      });
+    })
+    .then(() => ({
+		  pouch, db
+	  }));
 };
