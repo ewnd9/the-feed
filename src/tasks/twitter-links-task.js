5ea64e33534452fae1b2bac07c79fa615bc392ba
ewnd9 <ewndnine@gmail.com>
2016-02-18 22:59:52 +0800

    fix twitter-links task

--- src/tasks/twitter-links-task.js
+++ src/tasks/twitter-links-task.js
@@ -1,24 +1,41 @@
-import twitter from './../utils/twitter';
 import _ from 'lodash';
+import Twitter from 'twitter';
+import Promise from 'bluebird';
 import url from 'url';
 
-const task = (ignored) => {
-  return twitter.getAsync('statuses/home_timeline').then((result) => {
-    return result.filter((status) => {
-      return status.entities.urls &&
-             status.entities.urls.length > 0 &&
-             _.any(status.entities.urls, (_url) => ignored.indexOf(url.parse(_url.expanded_url).hostname) === -1);
+const task = ({
+  ignored,
+  consumer_key,
+  consumer_secret,
+  access_token_key,
+  access_token_secret
+}) => {
+  const twitter = Promise.promisifyAll(new Twitter({
+    consumer_key,
+    consumer_secret,
+    access_token_key,
+    access_token_secret
+  }));
+
+  return twitter
+    .getAsync('statuses/home_timeline')
+    .then(result => {
+      return result.filter((status) => {
+        return status.entities.urls &&
+               status.entities.urls.length > 0 &&
+               _.any(status.entities.urls, (_url) => (ignored || []).indexOf(url.parse(_url.expanded_url).hostname) === -1);
+      });
+    }).then(result => {
+      return result.map((item) => ({
+        id: item.id_str,
+        title: `@${item.user.screen_name}: ${item.text}`,
+        url: `https://twitter.com/${item.user.screen_name}/status/${item.id_str}`,
+        data: {
+          user_label: item.user.screen_name,
+          user_link: `https://twitter.com/${item.user.screen_name}`
+        }
+      }));
     });
-  }).then((result) => {
-    return result.map((item) => ({
-      id: item.id_str,
-      title: `@${item.user.screen_name}: ${item.text}`,
-      url: `https://twitter.com/${item.user.screen_name}/status/${item.id_str}`,
-      user_url: `https://twitter.com/${item.user.screen_name}`
-    }));
-  });
 };
 
-export default {
-  task
-};
+export default { task };
--- test/twitter-links-spec.js
+++ test/twitter-links-spec.js
@@ -6,12 +6,19 @@ test('follow specification', t => {
   t.true(typeof twitterTask.task === 'function');
 });
 
-// test('get links', async t => {
-//   const ignored = [
-//     'twitter.com'
-//   ];
-//
-//   const items = await twitterTask.task(ignored);
-//   console.log(items);
-//   setTimeout(() => t.fail(), 1000);
-// });
+test('get links', async t => {
+  const ignored = [
+    'twitter.com'
+  ];
+
+  const items = await twitterTask.task({
+    ignored,
+    consumer_key: process.env['TWITTER_CONSUMER_KEY'],
+    consumer_secret: process.env['TWITTER_CONSUMER_SECRET'],
+    access_token_key: process.env['TWITTER_ACCESS_TOKEN_KEY'],
+    access_token_secret: process.env['TWITTER_ACCESS_TOKEN_SECRET']
+  });
+
+  console.log(items);
+  setTimeout(() => t.fail(), 1000);
+});
