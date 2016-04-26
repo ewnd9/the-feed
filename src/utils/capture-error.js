b20e0f35ee52c7b5d0d14ff6b8a1670f3b0ef48d
ewnd9 <ewndnine@gmail.com>
2016-04-26 16:43:35 +0800

    check existence of opbeat config

--- src/utils/capture-error.js
+++ src/utils/capture-error.js
@@ -2,7 +2,7 @@ import config from '../config';
 
 let opbeat;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' && config.opbeat) {
   opbeat = require('opbeat').start({
     organizationId: config.opbeat.organizationId,
     appId: config.opbeat.appId,
