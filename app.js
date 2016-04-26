2646bd8dc4bad9b83fb4e5e0c89c37ed06eebe84
ewnd9 <ewndnine@gmail.com>
2016-04-27 07:39:41 +0800

    fix start script

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
+'use strict';
+
 require('source-map-support').install();
-// require('babel/register')({
-//   only: [__dirname + '/src']
-// });
-// require('./lib/cli');
-console.log(process.env)
+require('./dist/backend');
