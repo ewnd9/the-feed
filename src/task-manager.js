c79022c1cc821a03b4eac2ef7d978e140d8b6afa
ewnd9 <ewndnine@gmail.com>
2016-11-09 00:48:51 +0800

    refactor task manager

--- src/task-manager.js
+++ src/task-manager.js
@@ -1,6 +1,10 @@
 import Promise from 'bluebird';
 
-export const runJob = (db, job) => {
+export function TaskManager(db) {
+  this.db = new DB(db);
+}
+
+TaskManager.prototype.runJob = function(job) {
   const log = console.log.bind(console, job.name);
   log(new Date());
 
@@ -10,96 +14,137 @@ export const runJob = (db, job) => {
     overLimit: 0
   };
 
-  const currTask = require(`./tasks/${job.task}-task/${job.task}-task.js`).default;
-  let refineCount = 0;
+  const task = new Task(this.db, job, log, stats);
 
-  return currTask.task(job.params)
-    .then(items => {
-      stats.total = items.length;
-      return Promise.map(items, processItem);
-    })
-    .then(() => addUnseenCategoriesStatuses())
-    .catch(err => {
-      log(err, err.stack);
-    });
-
-  function processItem(item) {
-    item.meta = {
-      task: job.name,
-      seen: false
-    };
-
-    item.id = job.name + ':' + item.id.replace(/\W/g, '');
-
-    return db.find(item.id)
-      .then(() => {
-        stats.existed++;
-      }, err => {
-        if (err.reason === 'missing') {
-          return addIfNotFound(item);
-        } else {
-          log(err);
-        }
-      });
-  }
-
-  function addIfNotFound(item) {
-    if (currTask.refine) {
-      if (currTask.refineLimit) {
-        if (refineCount < currTask.refineLimit) {
-          refineCount++;
-
-          return currTask.refine(job.params, item)
-            .then(item => db.add(item))
-            .then(() => {
-              stats.added++;
-            }, err => {
-              log(err);
-            });
-        } else {
-          stats.overLimit++;
-        }
-      }
-    } else {
-      return db.add(item)
-        .then(() => {
-          stats.added++;
-        }, err => {
-          log(err);
-        });
-    }
-  }
-
-  function addUnseenCategoriesStatuses() {
-    log(stats);
-
-    if (stats.added > 0) {
-      const unseenStat = {
-        id: 'system-unseen:' + job.name,
-        unseen: true,
-        task: job.name
-      };
-
-      return db.find(unseenStat.id)
-        .then(item => {
-          item.unseen = true;
-          return db.add(item);
-        })
-        .catch(err => {
-          if (err.reason === 'missing') {
-            return db.add(unseenStat);
-          } else {
-            log(err);
-          }
-        });
-    }
-  }
+  return task
+    .run()
+    .then(() => this.addUnseenCategoriesStatuses(job, log, stats))
+    .catch(err => log(err));
 };
 
+TaskManager.prototype.addUnseenCategoriesStatuses = function(job, log, stats) {
+  log(stats);
+
+  if (stats.added > 0) {
+    const unseenStat = {
+      _id: 'system-unseen:' + job.name,
+      unseen: true,
+      task: job.name
+    };
+
+    return this.db.upsert(unseenStat, item => {
+      item.unseen = true;
+      return item;
+    });
+  }
+}
+
+function Task(db, job, log, stats) {
+  this.db = db;
+  this.job = job;
+  this.log = log;
+  this.stats = stats;
+  this.executor = require(`./tasks/${job.task}-task/${job.task}-task.js`).default;
+}
+
+Task.prototype.run = function() {
+  let refineCount = 0;
+
+  return this.executor
+    .task(this.job.params)
+    .then(items => {
+      this.stats.total = items.length;
+      return Promise.map(items, item => this.processItem(item));
+    })
+    .catch(err => {
+      this.log(err, err.stack);
+    });
+}
+
+Task.prototype.processItem = function(item) {
+  item.meta = {
+    task: this.job.name,
+    seen: false
+  };
+
+  item._id = this.job.name + ':' + item.id.replace(/\W/g, '') + '';
+
+  item.original_id = item.id;
+  delete item.id;
+
+  return this.db.upsert(
+    item,
+    data => {
+      this.stats.existed++;
+      return data;
+    },
+    this.addIfNotFound.bind(this)
+  );
+}
+
+Task.prototype.addIfNotFound = function(item) {
+  if (this.executor.refine) {
+    if (this.executor.refineLimit) {
+      if (refineCount < this.executor.refineLimit) {
+        refineCount++;
+
+        return this.executor
+          .refine(this.job.params, item)
+          .then(item => this.db.add(item))
+          .then(() => {
+            this.stats.added++;
+          }, err => {
+            log(err);
+          });
+      } else {
+        this.stats.overLimit++;
+      }
+    }
+  } else {
+    return this.db
+      .add(item)
+      .then(res => {
+        this.stats.added++;
+        return res;
+      }, err => {
+        log(err);
+      });
+  }
+}
+
+function DB(db) {
+  this.db = db;
+}
+
+DB.prototype.add = function(item) {
+  return this.db.add(item);
+};
+
+DB.prototype.upsert = function(item, upd = x => x, onNotFound) {
+  return this.db
+    .find(item._id)
+    .then(data => {
+      return this.db.add(upd(data));
+    })
+    .catch(err => {
+      if (err.name === 'Not found') {
+        if (onNotFound) {
+          return onNotFound(item);
+        } else {
+          return this.db.add(item);
+        }
+      }
+
+      return Promise.reject(err);
+    });
+}
+
 export default (db, jobs) => {
+  const manager = new TaskManager(db);
+
   jobs.forEach((job, i) => {
     setTimeout(() => {
-      const fn = runJob.bind(null, db, job);
+      const fn = manager.runJob.bind(manager, job);
       setInterval(fn, 1000 * 60 * job.interval);
       fn();
     }, i * 1000 * 10);
--- test/task-manager-spec.js
+++ test/task-manager-spec.js
@@ -1,23 +1,23 @@
 import test from 'ava';
 import 'babel-core/register';
 
-import { runJob } from '../src/task-manager';
+import { TaskManager } from '../src/task-manager';
 import dummyTask from '../src/tasks/dummy-task/dummy-task';
 
 import initDb from '../src/db';
 
-test.only('task manager', async t => {
+test('task manager', async t => {
   const { pouch, db } = await initDb(`/tmp/${Math.random()}`);
 
   const items = await dummyTask.task();
   t.ok(items.length > 0);
-  
-  const result = await runJob(db, { name: 'test-dummy', task: 'dummy' });
+
+  const manager = new TaskManager(db);
+  const result = await manager.runJob({ name: 'test-dummy', task: 'dummy' });
   t.ok(result.id === 'system-unseen:test-dummy');
 
   const docs = await pouch.allDocs({
     include_docs: true,
-    attachments: true,
     startkey: 'design_\uffff'
   });
 
